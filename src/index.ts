b24365669791d76dbab79411dc8d933d4df2d6d4
Christopher Gallego <ch.gallego06@gmail.com>
2024-05-17 16:26:04 -0500

    Updated exports

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// src/index.ts
+export * from './apollo/dataQuery';
+export * from './apollo/metadataQuery';
+export * from './apollo/reldataQuery';
+export * from './components/Form';
+export * from './components/FormContainer';
+export * from './components/Grid';
+export * from './components/TrayContainer';
+export * from './config/apollo';
+export * from './types/gridInterface';
