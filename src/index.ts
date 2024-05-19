8a1485f96c906dd9992e0f5b95117fb234817153
Christopher Gallego <ch.gallego06@gmail.com>
2024-05-19 16:32:04 -0500

    Added default imports

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './apollo/reldataQuery';
 export * from './components/Form';
 export * from './components/FormContainer';
 export * from './components/Grid';
-export * from './components/TrayContainer';
+export { default as TrayContainer } from './components/TrayContainer';
+export { default as QTFDashboard } from './components/Dashboard';
 export * from './config/apollo';
 export * from './types/gridInterface';
