93fb9b7658a2c7890a76f083fa6a013cee6841aa
Christopher Bryan Gallego Gomez <christopher.gallego@ant.gov.co>
2024-05-28 17:58:06 -0500

    Updated to include relationship data

--- src/apollo/metadataQuery.tsx
+++ src/apollo/metadataQuery.tsx
@@ -2,13 +2,16 @@ import { DocumentNode, gql } from "@apollo/client";
 
 export function getTableMetadata(): DocumentNode {
     return gql`
-        query GetTableMetadata($tableName: String!, $exceptions: String) {
-            tableMetadata(tableName: $tableName, exceptions: $exceptions) {
+        query GetTableMetadata($tableName: String!) {
+            tableMetadata(tableName: $tableName) {
                 column_name
-                column_default
                 data_type
-                is_nullable
                 character_maximum_length
+                column_default
+                is_nullable
+                is_reference
+                reference_table
+                reference_column
             }
         }
     `;
