ebd5a6e6795db7c50577851a99085ffe82e23599
Christopher Bryan Gallego Gomez <christopher.gallego@ant.gov.co>
2024-05-28 17:59:50 -0500

    Added exception

--- src/mapper/dtoMapper.tsx
+++ src/mapper/dtoMapper.tsx
@@ -1,6 +1,6 @@
 export function formToDto(formValues: any) {
     return Object.entries(formValues)
-        .filter(([key, value]) => value !== '' && value !== null && value !== undefined && key !== 't_id')
+        .filter(([key, value]) => value !== '' && value !== null && value !== undefined && key !== 't_id' && key !== 't_ili_tid')
         .map(([key, value]) => ({
             column_name: key,
             value: value
