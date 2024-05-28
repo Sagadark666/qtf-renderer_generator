01ab91ab38bd360d29dc93c96185abef5dbf1c1b
Christopher Bryan Gallego Gomez <christopher.gallego@ant.gov.co>
2024-05-28 08:26:05 -0500

    Added need

--- src/mapper/FieldMapper.tsx
+++ src/mapper/FieldMapper.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { TextField, NumberField, DateField, EmailField, CheckboxField } from '../components/FormFieldComponents';
 
-const fieldMapper = (dataType: string, name: string, maxLength: number, placeholder: string) => {
-  if (name === 't_id') {
-    return null;
-  }
+const fieldMapper = (dataType: string, name: string, maxLength: number, defaultValue: any, handleInputChange: (name: string, value: any) => void) => {
+    if (name === 't_id') {
+        return null;
+    }
 
-  const specialCases: any = {
-      documento_identidad: <NumberField name={name} maxLength={maxLength} placeholder={placeholder} />,
-      numero_celular: <NumberField name={name} maxLength={maxLength} placeholder={placeholder} />,
-      correo_electronico: <EmailField name={name} placeholder={placeholder} />,
-};
+    const specialCases: any = {
+        documento_identidad: <NumberField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        numero_celular: <NumberField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        correo_electronico: <EmailField name={name} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+    };
 
-if (specialCases[name]) {
-    return specialCases[name];
-}
+    if (specialCases[name]) {
+        return specialCases[name];
+    }
 
-const dataTypeCases: any = {
-    'character varying': <TextField name={name} maxLength={maxLength} placeholder={placeholder} />,
-    'text': <TextField name={name} maxLength={maxLength} placeholder={placeholder} />,
-    'number': <NumberField name={name} maxLength={maxLength} placeholder={placeholder} />,
-    'bigint': <NumberField name={name} maxLength={maxLength} placeholder={placeholder} />,
-    'int': <NumberField name={name} maxLength={maxLength} placeholder={placeholder} />,
-    'date': <DateField name={name} placeholder={placeholder} />,
-    'email': <EmailField name={name} placeholder={placeholder} />,
-    'checkbox': <CheckboxField name={name} />,
-};
+    const dataTypeCases: any = {
+        'character varying': <TextField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        'text': <TextField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        'number': <NumberField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        'bigint': <NumberField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        'int': <NumberField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        'date': <DateField name={name} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        'email': <EmailField name={name} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />,
+        'checkbox': <CheckboxField name={name} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.checked)} />,
+    };
 
-return dataTypeCases[dataType] || <TextField name={name} maxLength={maxLength} placeholder={placeholder} />;
+    return dataTypeCases[dataType] || <TextField name={name} maxLength={maxLength} defaultValue={defaultValue} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(name, e.target.value)} />;
 };
 
 export default fieldMapper;
