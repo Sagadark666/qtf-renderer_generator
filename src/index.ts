edd40906e256eedaef6107b12ef5725815f6ad80
Christopher Gallego <ch.gallego06@gmail.com>
2024-06-18 12:26:35 -0500

    Deleted unnecesary export

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,3 @@ export * from './components/Grid';
 export { default as TrayContainer } from './components/TrayContainer';
 export { default as QTFDashboard } from './components/Dashboard';
 export * from './config/apollo';
-export * from './types/gridInterface';
