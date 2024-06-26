abcc7587517fb6258a5c5be6dfcebd1e7b4406f6
Christopher Gallego <ch.gallego06@gmail.com>
2024-06-26 00:30:11 -0500

    Improved to allow install

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,22 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.ts', // Entry point of your library
+  entry: './src/index.ts',
+  mode: 'production',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd', // Universal Module Definition, to support CommonJS, AMD and global variable
-    globalObject: 'this', // To make UMD build available on both browser and Node.js
-    clean: true, // Clean the output directory before emit
+    filename: 'index.js',
+    library: 'qtfRenderer',
+    libraryTarget: 'umd',
+    globalObject: 'this',
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'],
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/, // Handle .ts and .tsx files
+        test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -26,24 +27,18 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
-  mode: 'development', // Change to 'production' for production builds
+  plugins: [new MiniCssExtractPlugin()],
   externals: {
-    // Exclude dependencies from the output bundle
     react: {
       commonjs: 'react',
       commonjs2: 'react',
-      amd: 'react',
+      amd: 'React',
       root: 'React',
     },
     'react-dom': {
       commonjs: 'react-dom',
       commonjs2: 'react-dom',
-      amd: 'react-dom',
+      amd: 'ReactDOM',
       root: 'ReactDOM',
     },
   },
